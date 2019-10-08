e70b43d37f55f6bfd72685d96fcd609a1a4a5214
DannyBoyNYC <daniel.deverell@gmail.com>
2019-10-07 21:48:18 -0400

    added recipes

--- app/js/recipes.js
+++ app/js/recipes.js
@@ -24,9 +24,31 @@ var apiData = [
   {
     name: 'Lasagna',
     regions: ['Italy', 'Spain', 'Greece'],
-    photo: 'pesto.jpg',
+    photo: 'lasagna.png',
     description:
-      'A sauce of crushed basil leaves, pine nuts, garlic, Parmesan cheese, and olive oil, typically served with pasta.',
+      'Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.',
+    directions: [
+      'Combine the basil, garlic, and pine nuts in a food processor and pulse until coarsely chopped. Add 1/2 cup of the oil and process until fully incorporated and smooth. Season with salt and pepper.',
+      'If using immediately, add all the remaining oil and pulse until smooth. Transfer the pesto to a large serving bowl and mix in the cheese.',
+      'If freezing, transfer to an air-tight container and drizzle remaining oil over the top. Freeze for up to 3 months. Thaw and stir in cheese.',
+    ],
+    ingredients: [
+      '2 cups pasta',
+      '2 cloves garlic',
+      '1/4 cup pine nuts',
+      '2/3 tomatoe sauce',
+      'Kosher salt and freshly ground black pepper, to taste',
+      '1/2 cup freshly grated Pecorino cheese',
+      '1 <abbr title="Pounds">lb</abbr> plain pasta',
+    ],
+  },
+
+  {
+    name: 'Pho Soup',
+    regions: ['Vietnam', 'Thailand', 'China'],
+    photo: 'pho.png',
+    description:
+      'Pho (pronounced "fuh") is the most popular food in Vietnam, often eaten for breakfast, lunch and dinner. It is made from a special broth that simmers for several hours infused with exotic spices and served over rice noodles with fresh herbs.',
     directions: [
       'Combine the basil, garlic, and pine nuts in a food processor and pulse until coarsely chopped. Add 1/2 cup of the oil and process until fully incorporated and smooth. Season with salt and pepper.',
       'If using immediately, add all the remaining oil and pulse until smooth. Transfer the pesto to a large serving bowl and mix in the cheese.',
@@ -44,11 +66,11 @@ var apiData = [
   },
 
   {
-    name: 'Pesto',
-    regions: ['Italy', 'France', 'Greece'],
-    photo: 'pesto.jpg',
+    name: 'Guacamole',
+    regions: ['Mexico', 'California', 'Guatemala'],
+    photo: 'guacamole.png',
     description:
-      'A sauce of crushed basil leaves, pine nuts, garlic, Parmesan cheese, and olive oil, typically served with pasta.',
+      'Guacamole is definitely a staple of Mexican cuisine. Even though Guacamole is pretty simple, it can be tough to get the perfect flavor - with this authentic Mexican guacamole recipe, though, you will be an expert in no time.',
     directions: [
       'Combine the basil, garlic, and pine nuts in a food processor and pulse until coarsely chopped. Add 1/2 cup of the oil and process until fully incorporated and smooth. Season with salt and pepper.',
       'If using immediately, add all the remaining oil and pulse until smooth. Transfer the pesto to a large serving bowl and mix in the cheese.',
@@ -66,33 +88,11 @@ var apiData = [
   },
 
   {
-    name: 'Pesto',
-    regions: ['Italy', 'France', 'Greece'],
-    photo: 'pesto.jpg',
+    name: 'Hamburger',
+    regions: ['United State', 'Germany', 'Canada'],
+    photo: 'hamburger.png',
     description:
-      'A sauce of crushed basil leaves, pine nuts, garlic, Parmesan cheese, and olive oil, typically served with pasta.',
-    directions: [
-      'Combine the basil, garlic, and pine nuts in a food processor and pulse until coarsely chopped. Add 1/2 cup of the oil and process until fully incorporated and smooth. Season with salt and pepper.',
-      'If using immediately, add all the remaining oil and pulse until smooth. Transfer the pesto to a large serving bowl and mix in the cheese.',
-      'If freezing, transfer to an air-tight container and drizzle remaining oil over the top. Freeze for up to 3 months. Thaw and stir in cheese.',
-    ],
-    ingredients: [
-      '2 cups packed fresh basil leaves',
-      '2 cloves garlic',
-      '1/4 cup pine nuts',
-      '2/3 cup extra-virgin olive oil',
-      'Kosher salt and freshly ground black pepper, to taste',
-      '1/2 cup freshly grated Pecorino cheese',
-      '1 <abbr title="Pounds">lb</abbr> plain pasta',
-    ],
-  },
-
-  {
-    name: 'Pesto',
-    regions: ['Italy', 'France', 'Greece'],
-    photo: 'pesto.jpg',
-    description:
-      'A sauce of crushed basil leaves, pine nuts, garlic, Parmesan cheese, and olive oil, typically served with pasta.',
+      'A Hamburger (often called a burger) is a type of sandwich in the form of  rounded bread sliced in half with its center filled with a patty which is usually ground beef, then topped with vegetables such as lettuce, tomatoes and onions.',
     directions: [
       'Combine the basil, garlic, and pine nuts in a food processor and pulse until coarsely chopped. Add 1/2 cup of the oil and process until fully incorporated and smooth. Season with salt and pepper.',
       'If using immediately, add all the remaining oil and pulse until smooth. Transfer the pesto to a large serving bowl and mix in the cheese.',

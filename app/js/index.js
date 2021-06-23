227bae3ce9ef531dc129063cc1d8cd740fc7025d
Sabrina Scatena <sabrina.scatena@gmail.com>
2021-06-22 20:08:39 -0400

    done

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,32 @@
+var modalOuter = document.querySelector('.modal-outer');
+var modalInner = document.querySelector('.modal');
+
+var betaContent = `
+<h3>Oooops!</h3>
+<p>Wow! Nothing works!<p>
+`;
+var buttonContent = `
+<h2>Coming Soon</h2>
+<p>This feature coming soon.<p>
+<a class="closer" href="#0">✖︎</a>
+`;
+
+function showPopover(event) {
+  if (event.target.matches('.beta')) {
+    modalInner.innerHTML = betaContent;
+    modalOuter.classList.add('open');
+  } else if (event.target.closest('nav ul')) {
+    modalInner.innerHTML = buttonContent;
+    modalOuter.classList.add('open');
+  } else if (event.target.matches('.closer, .modal-outer')) {
+    modalOuter.classList.remove('open');
+  } else return;
+  event.preventDefault();
+}
+
+document.addEventListener('click', showPopover);
+
+//  Mamas Spicy Basic
 const el = document.querySelector('h2');
 const basilChefs = ['mama', 'papa', 'baby'];
 const basilTexture = ['greasy', 'frozen', 'spicy'];
